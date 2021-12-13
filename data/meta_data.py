a5e9b387e5ce5e9dbef0f91fc556d361edad9d4a
Dan Gocan <dangocan@outlook.com>
2021-12-13 20:28:53 +0000

    Code refactoring and cleaning

--- data/meta_data.py
+++ data/meta_data.py
@@ -1,1 +1,135 @@
-{'all_data_length': 32561, 'valid_data_length': 30162, 'deleted_entries': 2399, 'train_data_length': 24130, 'test_data_length': 6032, 'created_on': '13/12, at 19:57:26', 'amount_of_categories': {'age': 7, 'workclass': 7, 'education_number': 16, 'marital_status': 7, 'occupation': 14, 'relationship': 6, 'race': 5, 'gender': 2, 'capital_gain': 6, 'capital_loss': 8, 'hours_per_week': 7, 'outcome': 2}, 'amount_of_values': {'age': {'65 and over': 975, '55-54 years old': 2849, '25-34 years old': 8040, '18-24 years old': 4541, 'Under 18': 328, '35-44 years old': 7807, '45-54 years old': 5621}, 'workclass': {'Local-gov': 2067, 'Private': 22285, 'Self-emp-not-inc': 2499, 'Federal-gov': 943, 'State-gov': 1279, 'Self-emp-inc': 1074, 'Without-pay': 14}, 'education_number': {9: 9840, 12: 1008, 4: 557, 10: 6678, 6: 820, 13: 5044, 8: 377, 14: 1626, 16: 375, 3: 288, 5: 455, 11: 1307, 7: 1048, 2: 151, 1: 45, 15: 542}, 'marital_status': {'Widowed': 827, 'Divorced': 4214, 'Never-married': 9726, 'Married-civ-spouse': 14064, 'Separated': 939, 'Married-spouse-absent': 370, 'Married-AF-spouse': 21}, 'occupation': {'Adm-clerical': 3721, 'Handlers-cleaners': 1350, 'Transport-moving': 1572, 'Sales': 3584, 'Farming-fishing': 989, 'Prof-specialty': 4038, 'Other-service': 3212, 'Machine-op-inspct': 1966, 'Tech-support': 911, 'Exec-managerial': 3992, 'Craft-repair': 4030, 'Protective-serv': 644, 'Priv-house-serv': 143, 'Armed-Forces': 9}, 'relationship': {'Not-in-family': 7726, 'Unmarried': 3212, 'Other-relative': 889, 'Own-child': 4466, 'Husband': 12462, 'Wife': 1406}, 'race': {'Black': 2817, 'White': 25932, 'Other': 231, 'Asian-Pac-Islander': 895, 'Amer-Indian-Eskimo': 286}, 'gender': {'Female': 9782, 'Male': 20379}, 'capital_gain': {'Capital gain 0': 27623, 'Capital gain under 12499.875': 1866, 'Capital gain under 24999.75': 474, 'Capital gain under 100000': 148, 'Capital gain under 37499.625': 48, 'Capital gain under 49999.5': 2}, 'capital_loss': {'Capital loss 0': 28734, 'Capital loss under 3267.0': 200, 'Capital loss under 1633.5': 258, 'Capital loss under 2178.0': 928, 'Capital loss under 544.5': 9, 'Capital loss under 1089.0': 25, 'Capital loss under 4357': 3, 'Capital loss under 3811.5': 4}, 'hours_per_week': {'Hours per week under 24.75': 2029, 'Hours per week under 49.5': 17748, 'Hours per week under 74.25': 5798, 'Hours per week under 37.125': 3534, 'Hours per week under 86.625': 259, 'Hours per week under 12.375': 657, 'Hours per week under 100': 136}, 'outcome': {False: 22653, True: 7508}}}
+{
+    'all_data_length': 32561,
+    'valid_data_length': 30162,
+    'deleted_entries': 2399,
+    'train_data_length': 24130,
+    'test_data_length': 6032,
+    'created_on': '13/12, at 19:57:26',
+    'amount_of_categories': {
+        'age': 7,
+        'workclass': 7,
+        'education_number': 16,
+        'marital_status': 7,
+        'occupation': 14,
+        'relationship': 6,
+        'race': 5,
+        'gender': 2,
+        'capital_gain': 6,
+        'capital_loss': 8,
+        'hours_per_week': 7,
+        'outcome': 2
+    },
+    'amount_of_values': {
+        'age': {
+            '65 and over': 975,
+            '55-54 years old': 2849,
+            '25-34 years old': 8040,
+            '18-24 years old': 4541,
+            'Under 18': 328,
+            '35-44 years old': 7807,
+            '45-54 years old': 5621
+        },
+        'workclass': {
+            'Local-gov': 2067,
+            'Private': 22285,
+            'Self-emp-not-inc': 2499,
+            'Federal-gov': 943,
+            'State-gov': 1279,
+            'Self-emp-inc': 1074,
+            'Without-pay': 14
+        },
+        'education_number': {
+            9: 9840,
+            12: 1008,
+            4: 557,
+            10: 6678,
+            6: 820,
+            13: 5044,
+            8: 377,
+            14: 1626,
+            16: 375,
+            3: 288,
+            5: 455,
+            11: 1307,
+            7: 1048,
+            2: 151,
+            1: 45,
+            15: 542
+        },
+        'marital_status': {
+            'Widowed': 827,
+            'Divorced': 4214,
+            'Never-married': 9726,
+            'Married-civ-spouse': 14064,
+            'Separated': 939,
+            'Married-spouse-absent': 370,
+            'Married-AF-spouse': 21
+        },
+        'occupation': {
+            'Adm-clerical': 3721,
+            'Handlers-cleaners': 1350,
+            'Transport-moving': 1572,
+            'Sales': 3584,
+            'Farming-fishing': 989,
+            'Prof-specialty': 4038,
+            'Other-service': 3212,
+            'Machine-op-inspct': 1966,
+            'Tech-support': 911,
+            'Exec-managerial': 3992,
+            'Craft-repair': 4030,
+            'Protective-serv': 644,
+            'Priv-house-serv': 143,
+            'Armed-Forces': 9
+        },
+        'relationship': {
+            'Not-in-family': 7726,
+            'Unmarried': 3212,
+            'Other-relative': 889,
+            'Own-child': 4466,
+            'Husband': 12462,
+            'Wife': 1406
+        },
+        'race': {
+            'Black': 2817,
+            'White': 25932,
+            'Other': 231,
+            'Asian-Pac-Islander': 895,
+            'Amer-Indian-Eskimo': 286
+        },
+        'gender': {
+            'Female': 9782,
+            'Male': 20379
+        },
+        'capital_gain': {
+            'Capital gain 0': 27623,
+            'Capital gain under 12499.875': 1866,
+            'Capital gain under 24999.75': 474,
+            'Capital gain under 100000': 148,
+            'Capital gain under 37499.625': 48,
+            'Capital gain under 49999.5': 2
+        },
+        'capital_loss': {
+            'Capital loss 0': 28734,
+            'Capital loss under 3267.0': 200,
+            'Capital loss under 1633.5': 258,
+            'Capital loss under 2178.0': 928,
+            'Capital loss under 544.5': 9,
+            'Capital loss under 1089.0': 25,
+            'Capital loss under 4357': 3,
+            'Capital loss under 3811.5': 4
+        },
+        'hours_per_week': {
+            'Hours per week under 24.75': 2029,
+            'Hours per week under 49.5': 17748,
+            'Hours per week under 74.25': 5798,
+            'Hours per week under 37.125': 3534,
+            'Hours per week under 86.625': 259,
+            'Hours per week under 12.375': 657,
+            'Hours per week under 100': 136
+        },
+        'outcome': {
+            False: 22653,
+            True: 7508
+        }
+    }
+}
